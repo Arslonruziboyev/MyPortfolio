bc09b62d5ccf2b24bf514956a2e9876b0eb64e25
02arslon <arslonruziboyev@gmail.com>
2023-04-06 19:04:29 +0900

    changed

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,15 +1,45 @@
-import { Link } from 'react-router-dom'
-import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { Link, NavLink } from "react-router-dom";
+import "./index.scss";
+import LogoS from "../../assets/images/logo-s.png";
+import LogoSubtitle from "../../assets/images/logo_sub.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBitcoin,
+  faChrome,
+  faHouzz,
+} from "@fortawesome/free-brands-svg-icons";
+import { FaEnvelope, FaHome, FaUser } from "react-icons/fa";
 
-const Sidebar = () =>{
-    return <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoS} alt='logo' />
-            <img src={LogoSubtitle} alt='Subtitle Logo' />
-        </Link>
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="Subtitle Logo" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeClassName="active" to="/">
+          <FaHome color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeClassName="active"
+          className="about-link"
+          to="/about"
+        >
+          <FaUser color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeClassName="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FaEnvelope color="#4d4d4e" />
+        </NavLink>
+      </nav>
     </div>
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
